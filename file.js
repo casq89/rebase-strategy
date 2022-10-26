d5c51d32be87424ad80fc6a84398322e3086a801
Camillo Sanchez <casq89@gmail.com>
2022-10-26 14:16:41 -0500

    commit 4 in 1

--- file.js
+++ file.js
@@ -3,8 +3,9 @@ console.log("greet from b");
 console.log("second greet from rebase");
 console.log("greet from a");
 console.log("greet from a");
-<<<<<<< HEAD
 console.log("greet from aaa");
-=======
 console.log("greet from bb");
->>>>>>> new change b
+console.log("feature a");
+console.log("feature b");
+console.log("add somethign main");
+console.log("add somethign main 2");
