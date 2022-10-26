c7c351d93f4661936031753b5467f1acc954c965
Camillo Sanchez <casq89@gmail.com>
2022-10-26 14:16:41 -0500

    feature a

--- file.js
+++ file.js
@@ -3,8 +3,6 @@ console.log("greet from b");
 console.log("second greet from rebase");
 console.log("greet from a");
 console.log("greet from a");
-<<<<<<< HEAD
 console.log("greet from aaa");
-=======
 console.log("greet from bb");
->>>>>>> new change b
+console.log("feature a");
