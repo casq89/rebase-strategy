a828d3f97f444c4a25897aa261745d156953536d
Camillo Sanchez <casq89@gmail.com>
2022-10-26 13:56:05 -0500

    commit a + b

--- file.js
+++ file.js
@@ -3,3 +3,8 @@ console.log("greet from b");
 console.log("second greet from rebase");
 console.log("greet from a");
 console.log("greet from a");
+<<<<<<< HEAD
+console.log("greet from aaa");
+=======
+console.log("greet from bb");
+>>>>>>> new change b
