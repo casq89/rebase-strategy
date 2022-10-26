531b12d63115818ea187dbcfde2614499b79e0f2
Camillo Sanchez <casq89@gmail.com>
2022-10-26 12:09:32 -0500

    greet merge
    greet commit
    
    greet A

--- file.js
+++ file.js
@@ -1,5 +1,7 @@
 console.log("first greet from rebase");
 console.log("greet from b");
+console.log("greet from merge");
+console.log("greet from commit");
 console.log("second greet from rebase");
 console.log("greet from a");
 console.log("greet from a");
