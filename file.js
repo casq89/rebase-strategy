165ad9923e8125f77ec0ea8ee385748493e45786
Camillo Sanchez <casq89@gmail.com>
2022-10-28 07:54:50 -0500

    initial commit

--- file.js
+++ file.js
@@ -1,6 +1,1 @@
-console.log("first greet from rebase");
-console.log("greet from b");
-console.log("second greet from rebase");
-console.log("greet from a");
-console.log("greet from a");
-console.log("main 2");
+console.log("initial commit");
