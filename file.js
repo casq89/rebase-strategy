06bb26a80046f46889a237f7a3c0e38681070ef2
Camillo Sanchez <casq89@gmail.com>
2022-10-28 08:37:28 -0500

    from public 3

--- file.js
+++ file.js
@@ -1,5 +1,6 @@
 console.log("initial commit");
 console.log("from public dev 1");
 console.log("from public dev 2");
+console.log("from public dev 3");
 console.log("from a");
 console.log("from b");
